28eed80c9028c84608a792e011ac904e7cad4f74
M.A.S <m.alex.vi.silva@gmail.com>
2025-03-11 21:03:56 -0400

    feat: Query Strings, Request and Response

--- FirstApplication/FirstApplication/Program.cs
+++ FirstApplication/FirstApplication/Program.cs
@@ -9,9 +9,45 @@ internal class Program
         app.Run(async (HttpContext context) => {
 
             var path = context.Request.Path.ToString().ToLower();
+            var metodo = context.Request.Method;
 
                 switch (path)
                 {
+                    case "/query-string":
+                    if(context.Request.Method == "GET")
+                    {
+                        if (context.Request.Query.ContainsKey("id"))
+                        {
+
+                            string queryParamOne = context.Request.Query["id"];
+                            string queryParamOneName = context.Request.Query.GetType().Name;
+
+
+                            string queryParamTwo = context.Request.Query["name"];
+                            string queryParamTwoName = context.Request.Query.GetType().Name;
+
+                            await context.Response.WriteAsync(
+                                $"O valor do primeiro QueryParam (id) é: {queryParamOne}\n" +
+                                $"O valor do segundo QueryParam (name) é: {queryParamTwo}"
+                            );
+                        }
+                    }
+                    break;
+
+                    case "/http-request":
+
+                    await context.Response.WriteAsync($"<p>O caminho sendo utilizado agora e: {path}</p>");
+                    await context.Response.WriteAsync($"<p>O metodo sendo utilizado para a rota {path} e: {metodo}</p>");
+                    break;
+                    case "/headers":
+                    // Podemos alterar, adicionar ou remover headers
+                    context.Response.Headers["Authorization"] = "eylkd3h9uhf9hf92uhf2o3f2";
+                    context.Response.Headers.ContentType = "text/html";
+
+                    await context.Response.WriteAsync("<h1>Um texto em h1 </h1>");
+
+                    break;
+
                     case "/400":
                         context.Response.StatusCode = 400;
                         await context.Response.WriteAsync("Erro 400 - Bad Request");
