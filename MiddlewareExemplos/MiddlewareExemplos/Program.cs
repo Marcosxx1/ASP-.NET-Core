bd025ac7ea987a0ccb8c2f47e99ccc70a3ba4294
M.A.S <m.alex.vi.silva@gmail.com>
2025-03-13 19:02:14 -0400

    feat: Extension method for middleware

--- MiddlewareExemplos/MiddlewareExemplos/Program.cs
+++ MiddlewareExemplos/MiddlewareExemplos/Program.cs
@@ -1,8 +1,11 @@
+using MiddlewareExemplos.middleware;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddTransient<Middlewares>();
         var app = builder.Build();
         
         app.Use(async (HttpContext context, RequestDelegate next) =>
@@ -16,6 +19,8 @@ internal class Program
             await context.Response.WriteAsync(" | Ola do segundo middleware!! ");
             await next(context);
         });
+
+        app.UseCustomMiddleware();
 
         app.Use(async(HttpContext context, RequestDelegate next) =>
         {
---
+++ MiddlewareExemplos/MiddlewareExemplos/middleware/Middlewares.cs
@@ -0,0 +1,26 @@
+﻿
+using MiddlewareExemplos.middleware;
+
+namespace MiddlewareExemplos.middleware
+{
+    public class Middlewares : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync(" | Ola de dentro do Middleware customizado! 1 ");
+            await next(context);
+            await context.Response.WriteAsync(" | Ola de dentro do Middleware customizado! 2 ");
+
+        }
+    }
+}
+
+public static class CustomMiddlewareExemplo
+{
+    public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder app)
+    {
+
+        return app.UseMiddleware<Middlewares>();
+    }
+
+}
